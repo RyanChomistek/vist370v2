06b0da72a550e5d03951b9db4516f0b8ba55d352
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-09-06 20:59:16 -0500

    asd

---
+++ Assets/Scripts/SceneTransitionButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionButton : MonoBehaviour {
+    public string NextScene;
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+}
