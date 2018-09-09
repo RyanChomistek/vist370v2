34f1e909d9040f245b55049071bc82bdaab913b6
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-09-09 13:24:45 -0500

    asd

---
+++ Assets/InteractableElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableElement : MonoBehaviour {
+
+	void Start () {
+        transform.LookAt(Camera.main.transform);
+
+        transform.RotateAround(transform.position, transform.up, 180f);
+        transform.position = transform.position.normalized * 4;
+    }
+	
+}
